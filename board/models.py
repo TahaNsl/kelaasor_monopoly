1c565710213a8837bccc33ee997b4e6b8719e1c2
TaHa <tahanasrollahii@gmail.com>
2025-09-18 19:05:33 +0330

    move Card model to board app and remove card app

--- board/models.py
+++ board/models.py
@@ -33,3 +33,15 @@ class Property(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Card(models.Model):
+    CARD_TYPES = [
+        ("CHANCE", "Chance"),
+        ("CHEST", "Community Chest"),
+    ]
+    type = models.CharField(max_length=10, choices=CARD_TYPES)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.type} - {self.description[:30]}"
